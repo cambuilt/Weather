683a6e929730db8cbbd6d6174e2df915499ee4b4
Cameron Conway <cconway@cambuilt.com>
2015-07-30 11:05:20 -0400

    First table view controller.

---
+++ Weather/CityTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CityTableViewController.swift
+//  Weather
+//
+//  Created by Cameron Conway on 7/30/15.
+//  Copyright © 2015 Cam-Built Programming Plus. All rights reserved.
+//
+
+import Foundation
